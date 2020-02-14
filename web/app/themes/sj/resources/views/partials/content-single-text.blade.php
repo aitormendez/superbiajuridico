e2a775d73d317ca5b1b95b3edd4a3f2d2efbb103
aitor <aitor@e451.net>
2020-02-14 17:08:53 +0100

    Restaurando tema. Single text

--- web/app/themes/sj/resources/views/partials/content-single-text.blade.php
+++ web/app/themes/sj/resources/views/partials/content-single-text.blade.php
@@ -1,63 +1,73 @@
-<article  {{ post_class() }}>
+<div class="container">
+  <div class="row">
+    <article {{ post_class() }}>
 
-  <header>
-    <h1 class="entry-title">{!! get_the_title() !!}</h1>
-    @include('partials/entry-meta')
-  </header>
-  <div class="entry-content">
-    @if (has_excerpt())
-      <div class="excerpt">
-        <h2 class="epi-meta">{{ __('En breve', 'sage') }}</h2>
-          @php the_excerpt() @endphp
+      <header class="mx-4 mx-md-0 mt-5 mb-4">
+        <h1 class="entry-title mb-4">{!! get_the_title() !!}</h1>
+        @include('partials/entry-meta')
+      </header>
+      <div class="mx-4 mx-md-0 pb-5">
+        @if (has_excerpt())
+          <div class="excerpt p-3">
+            <h2 class="epi-meta">{{ __('En breve', 'sage') }}</h2>
+              @php the_excerpt() @endphp
+          </div>
+        @endif
+        @php
+          $sentencias = get_field('sentencias_asociadas');
+        @endphp
+        @if ($sentencias)
+          <div class="sentencias p-3 mb-4">
+            <h2 class="epi-meta mb-3">{{ __('Sentencias relacionadas', 'sage')}}</h2>
+              <ul>
+                @foreach ($sentencias as $sentencia)
+                  <li class="d-md-flex">
+                    @php
+                      $pdf = get_field('pdf', $sentencia->ID);
+                      $nombre = get_field('nombre', $sentencia->ID);
+                      $url_externa = get_field('url_externa', $sentencia->ID);
+                      $sentencia_no = get_field('sentencia_no', $sentencia->ID);
+                      $tribunal = get_field('tribunal', $sentencia->ID);
+                      $fecha= get_field('fecha', false, false);
+                      $fecha_datetime = new DateTime($fecha);
+                      $anio = $fecha_datetime->format('Y');
+                    @endphp
+
+                    @if ($sentencia_no == '')
+                      @php $identificador = $fecha_datetime->format('j/n/Y'); @endphp
+                    @else
+                      @php $identificador = $sentencia_no . '/' . $fecha_datetime->format('Y'); @endphp
+                    @endif
+
+                    @if ($nombre == '')
+                      @php
+                        $nombre = 'Enlace';
+                      @endphp
+                    @endif
+
+                    <strong>{{ $tribunal . '-' . $identificador }}.</strong>
+
+                    <div class="der">
+                      <a href="{{ get_permalink($sentencia->ID) }}"> {{ $nombre }} </a>
+                      @if ($pdf)
+                        | <a href="{{ $pdf['url'] }}" target="_blank" alt="{{ $pdf['title'] }}">
+                          PDF</a>
+                      @endif
+                      @if ($url_externa)
+                      |<a href="{{ $url_externa }}" target="_blank"> Ver en Cendoj</a>
+                      @endif
+                    </div>
+
+
+                  </li>
+                @endforeach
+              </ul>
+          </div>
+        @endif
+        <div class="entry-content">
+          @php the_content() @endphp
+        </div>
       </div>
-    @endif
-    @php
-      $sentencias = get_field('sentencias_asociadas');
-    @endphp
-    @if ($sentencias)
-      <div class="sentencias">
-        <h2 class="epi-meta">{{ __('Sentencias relacionadas', 'sage')}}</h2>
-          <ul>
-            @foreach ($sentencias as $sentencia)
-              <li>
-                @php
-                  $pdf = get_field('pdf', $sentencia->ID);
-                  $nombre = get_field('nombre', $sentencia->ID);
-                  $url_externa = get_field('url_externa', $sentencia->ID);
-                  $sentencia_no = get_field('sentencia_no', $sentencia->ID);
-                  $tribunal = get_field('tribunal', $sentencia->ID);
-                  $fecha= get_field('fecha', false, false);
-                  $fecha_datetime = new DateTime($fecha);
-                  $anio = $fecha_datetime->format('Y');
-                @endphp
-
-                @if ($sentencia_no == '')
-                  @php $identificador = $fecha_datetime->format('j/n/Y'); @endphp
-                @else
-                  @php $identificador = $sentencia_no . '/' . $fecha_datetime->format('Y'); @endphp
-                @endif
-
-                @if ($nombre == '')
-                  @php
-                    $nombre = 'Enlace';
-                  @endphp
-                @endif
-
-
-                <span class="codigo"><strong>{{ $tribunal . '-' . $identificador }}</strong>. <a href="{{ get_permalink($sentencia->ID) }}"> {{ $nombre }} </a></span>
-                @if ($pdf)
-                  | <a href="{{ $pdf['url'] }}" target="_blank" alt="{{ $pdf['title'] }}">PDF</a>
-                @endif
-                @if ($url_externa)
-                |<a href="{{ $url_externa }}" target="_blank"> Ver en Cendoj</a>
-                @endif
-              </li>
-            @endforeach
-          </ul>
-      </div>
-    @endif
-
-    @php the_content() @endphp
+    </article>
   </div>
-  @php comments_template('/partials/comments.blade.php') @endphp
-</article>
+</div>
