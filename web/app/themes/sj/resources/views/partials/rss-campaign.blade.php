1b654e3cd6945436136f380e00e1becb8bb96464
aitor <aitor@e451.net>
2020-02-17 11:26:27 +0100

    Pruebas feed

--- web/app/themes/sj/resources/views/partials/rss-campaign.blade.php
+++ web/app/themes/sj/resources/views/partials/rss-campaign.blade.php
@@ -98,7 +98,8 @@ do_action( 'rss_tag_pre', 'rss2' );
     <?php the_category_rss( 'rss2' ); ?>
 
     @if (has_post_thumbnail())
-    <media:content url="{{ get_the_post_thumbnail_url(get_the_ID(),'large') }}" medium="image">
+    <media:content url="{{ get_the_post_thumbnail_url(get_the_ID(),'large') }}" medium="image" />
+
     @endif
 
 		<guid isPermaLink="false"><?php the_guid(); ?></guid>
