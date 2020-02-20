4b3defbdb8bdf941d7915ea8dfeac26489749a7a
aitor <aitor@e451.net>
2020-02-20 13:56:19 +0100

    Pruebas feed RSS

--- web/app/themes/sj/app/Controllers/App.php
+++ web/app/themes/sj/app/Controllers/App.php
@@ -72,4 +72,16 @@ class App extends Controller
         return get_the_title();
     }
 
+    function pagNewsletter() {
+        $page = get_page_by_path( 'newsletter' );
+        $trid = apply_filters( 'wpml_element_trid', NULL, $page->ID, 'post_page' );
+        $esta = apply_filters( 'wpml_element_trid', NULL, get_the_id(), 'post_page' );
+
+        if ($esta === $trid) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
